19ff74c2e0a86b88fcf43b05a5468bb04a4fb9b2
Richard <erenmalmrichard@gmail.com>
2024-09-14 09:00:22 +0200

    Polymorfism uppgift klar

---
+++ Polymorfism/Fighter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorfism
+{
+    class Fighter
+    {
+        public string name;
+
+        //skapar en virtual metod vilket betyder att subklasserna kan välja att behålla matoden som den är eller overridea den
+        public virtual void attack()
+        {
+            Console.WriteLine(name + " does a jab,cross!");
+        }
+    }
+
+    class Khabib : Fighter
+    {
+        //overridear metoden vilket betyder att denna versionen kommer att köras istället för original versionen av khabib objekt
+        public override void attack()
+        {
+            Console.WriteLine("Khabib pushes you against the fence and secures a takedwon!");
+        }
+    }
+}
--- Polymorfism/Program.cs
+++ Polymorfism/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Fighter fighter1 = new Fighter();
+            fighter1.name = "Francis Nganou";
+            //objekt av typen Fighter utför standard versionen av attack metoden
+            fighter1.attack();
+
+            Console.WriteLine("");
+
+            Khabib khabib1 = new Khabib();
+            //objekt av typen khabib utför den ändrade versionan av attack metoden
+            khabib1.attack();
+
+            Console.ReadLine();
+            
         }
     }
 }
