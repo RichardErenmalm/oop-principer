c96bbbb817851593048eadee00515f44d112ce30
Richard <erenmalmrichard@gmail.com>
2024-09-14 12:18:20 +0200

    gjorde klart abstraktion uppgiften

---
+++ Abstraktion/Charachter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraktion
+{
+    //skapar en abstrakt klass som inte går att skapa ett objekt utav
+    //abstrakta klasser fungerar enbart som en mall för subklasserna
+    //I detta exemplet tänker jag mig ett tv spel. I spel så kan man inte välja att vara en "charachter", utan...
+    //man måste välja en specifik klass som orc eller wizard. Men klasserna är uppbyggda på samma struktur
+    //de har alla tex ett namn, en ordinarie attack och en unik special attack
+    //därför har man den abstrakta klassen som en mall för alla subklasser
+    abstract class Charachter
+    {
+        public string name;
+
+        //en vanlig klass som är samma för alla subklasser
+        public void attack()
+        {
+            Console.WriteLine(name + " attacks");
+        }
+
+        //en abstrakt klass som varje subklass måste overridea och ange vad denna klass ska göra
+        public abstract void special_attack();
+    }
+
+    class Orc : Charachter 
+    {
+        //anger vad orcens special attack är
+        public override void special_attack()
+        {
+            Console.WriteLine(name + " uses his twohanded axe to deal a devastating blow!");   
+        }
+
+    }
+
+    class Wizard : Charachter
+    {
+        //anger vad wizards special attack är
+        public override void special_attack() 
+        {
+            Console.WriteLine(name + " uses his staff to shoot fire at the enemy!");
+        }       
+    }
+
+}
--- Abstraktion/Program.cs
+++ Abstraktion/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Orc orc = new Orc();
+            orc.name = "MR orc";
+
+            //orc objektet utför attackmetoden som skapades i den abstrakta klassen
+            orc.attack();
+            //orc objektet utför special attack metoden som skapades i charachter klassen men...
+            //bestämdes hur den skulle utföras i orc klassen
+            orc.special_attack();
+
+            Console.WriteLine();
+
+            Wizard wizard = new Wizard();
+            wizard.name = "Gandalf";
+
+            //wizard objektet utför attackmetoden som specifierades i den abstrakta klassen
+            wizard.attack();
+            //wizard objektet utför special attack metoden som skapades i charachter klassen men...
+            //bestämdes hur den skulle utföras i wizard klassen
+            wizard.special_attack();
+
+
+            Console.ReadLine();
         }
     }
 }
